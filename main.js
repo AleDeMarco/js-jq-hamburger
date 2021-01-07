98c2ca6d1b1b8911360de22b8f617cc157cec953
Alessandro De Marco <lookatde.lad@gmail.com>
2021-01-07 13:36:42 +0100

    Esercizio terminato

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+  $(".fa-bars").click(function(){
+    $(".hamburger-menu").css("display", "block");
+  });
+
+  $(".fa-times").click(function(){
+    $(".hamburger-menu").css("display", "none");
+  });
+
+});
